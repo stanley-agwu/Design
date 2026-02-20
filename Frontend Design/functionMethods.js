6334cfc96a7abf1360909fc1b40606a152f4983d
stanley-agwu <stanleyagwu2016@gmail.com>
2026-02-20 23:32:23 +0100

    feat(Design): Frontend Design - Function methods - apply - Javascript

--- Frontend Design/functionMethods.js
+++ Frontend Design/functionMethods.js
@@ -21,3 +21,21 @@ function sayTemperature(temperature){
 console.log(sayTemperature(32))
 console.log(sayTemperature.call(person, 32))
 console.log(this)
+
+// 2. Function.prototype.apply
+// apply() -> functionName.apply(thisArg, [arg1, arg2, ...])
+
+// Works just like call(), but arguments are passed as an array.
+/*
+1. Calls the function immediately.
+2. Arguments are passed as an array.
+3. Explicitly sets this.
+*/
+
+function sayWeather(temperature, windSpeed, humidity){
+    return `Hello ${this.name}!, the weather says, temperature is 
+        ${temperature}°C, wind speed is ${windSpeed} and a humidity of ${humidity}.`
+}
+
+console.log(sayWeather(32, '42mph', '30 g/m³'))
+console.log(sayWeather.apply(person, [32, '42mph', '30 g/m³']))
