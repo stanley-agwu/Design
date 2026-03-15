0213bf198822e59d291937ef419d9a9252e2a30a
stanley-agwu <stanleyagwu2016@gmail.com>
2026-03-15 23:43:07 +0100

    feat(Design): Data Structure Design - Implement Stack using Queues - Python

---
+++ Data Structure Design/stack_using_queues.py
@@ -0,0 +1,77 @@
+# Implement Stack using Queues
+
+"""
+Implement a last-in-first-out (LIFO) stack using only two queues.
+The implemented stack should support all the functions of a normal stack 
+(push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only 
+push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively.
+You may simulate a queue using a list or deque (double-ended queue) 
+as long as you use only a queue's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, top, and empty.
+All the calls to pop and top are valid.
+"""
+
+# Make push() costly so that pop() and top() are O(1)
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        # push to q2
+        self.q2.append(x)
+
+        # move all elements from q1 → q2
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+
+        # swap q1 and q2
+        self.q1, self.q2 = self.q2, self.q1
+
+    def pop(self) -> int:
+        return self.q1.popleft()
+
+    def top(self) -> int:
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        return not self.q1
+
+# push O(n), pop O(1), top O(1), empty O(1) - Time complexity
+# O(n) - Space complexity
