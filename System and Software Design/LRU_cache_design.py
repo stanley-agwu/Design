fe49ada8e2c94cca60802b11e48829bf37ce3b65
stanley-agwu <stanleyagwu2016@gmail.com>
2026-04-06 23:24:12 +0200

    feat(Design): System and Software Design - Cache System Design - LRU - Python

--- System and Software Design/LRU_cache_design.py
+++ System and Software Design/LRU_cache_design.py
@@ -5,6 +5,43 @@
 # https://en.wikipedia.org/wiki/Cache_replacement_policies#LRU
 # The functions get and put must each run in O(1) average time complexity.
 
+"""
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+Example 1:
+
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
+
+Constraints:
+
+1 <= capacity <= 3000
+0 <= key <= 104
+0 <= value <= 105
+At most 2 * 105 calls will be made to get and put.
+"""
 
 # Data structure
 """
@@ -53,6 +90,8 @@ class LRUcache:
         prev_node, next_node = node.prev, node.next
         prev_node.next = next_node
         next_node.prev = prev_node
+        node.prev = node.next = None
+        return node
     
     def _add_to_front(self, node: Node) -> Node:
         # Insert node just after dummy head
@@ -61,14 +100,16 @@ class LRUcache:
         node.prev = self.head
         node.next = current_mru
         current_mru.prev = node
+        return node
 
     def _move_node_to_front(self, node: Node) -> None:
         self._remove_node(node)
         self._add_to_front(node)
+        return node
     
     def _pop_lru(self) -> Node:
         lru_node = self.tail.prev
-        self._remove_node(lru_node.key)
+        self._remove_node(lru_node)
         return lru_node
     
     # cache API
@@ -86,6 +127,7 @@ class LRUcache:
 
         if node:
             node.val = value
+            self.map[key] = node
             self._move_node_to_front(node)
             return None
 
